af22da1e140001dd37e6d866dc61c69a759c716e
romophic <romophic@users.noreply.github.com>
2024-10-16 09:52:14 +0000

    deploy: c5ecbe65dc288e874c8b3bc2074c5d6e95dfe023

---
+++ _next/static/chunks/app/page-4a25d9b0ba9269c0.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{5288:function(n,e,u){Promise.resolve().then(u.t.bind(u,2446,23))},2446:function(){}},function(n){n.O(0,[329,971,117,744],function(){return n(n.s=5288)}),_N_E=n.O()}]);
